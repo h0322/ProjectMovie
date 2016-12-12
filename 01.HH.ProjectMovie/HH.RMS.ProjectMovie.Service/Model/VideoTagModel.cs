2df0eb89b1b7dc23faec857d167b1126fc78b84f
henry <Henry.HUANG@edenred.com>
2016-12-12 16:13:28 +0800

    20161212

---
+++ 01.HH.ProjectMovie/HH.RMS.ProjectMovie.Service/Model/VideoTagModel.cs
@@ -0,0 +1,26 @@
+﻿using HH.RMS.Common.Attributes;
+using HH.RMS.Entity;
+using Nelibur.ObjectMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HH.ProjectMovie.Service.Model
+{
+    public class VideoTagModel
+    {
+        public string tagName { get; set; }
+        public string description { get; set; }
+        public decimal tagNum { get; set; }
+        public static T ModelMapper<T>(object entity)
+        {
+            return TinyMapper.Map<T>(entity);
+        }
+        public static T EntityMapper<T>(object model)
+        {
+            return TinyMapper.Map<T>(model);
+        }
+    }
+}
